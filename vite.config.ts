173f2d9413d6a83b7d1ed442dbb64566c6b03040
vecpeng <841298391cp@gmail.com>
2022-05-09 21:08:02 +0800

    chore: add alias config

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,18 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import viteEslint from 'vite-plugin-eslint'
 import viteStylelint from '@amatlash/vite-plugin-stylelint'
+import * as path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     viteEslint(),
     viteStylelint({ exclude: /windicss|node_modules/ })
-  ]
+  ],
+  resolve: {
+    // 别名配置，可以使用绝地路径引入src文件夹下面的文件
+    alias: {
+      '@/': path.join(__dirname, 'src/')
+    }
+  }
 })
