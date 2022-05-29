d89c9c8f0a168018219fddfae93f2483e7688ad7
vecpeng <841298391@qq.com>
2022-05-29 20:29:04 +0800

    feat: useDrag in midcanvas

--- src/utils/renderJson.tsx
+++ src/utils/renderJson.tsx
@@ -1,32 +1,61 @@
 import React, { CSSProperties } from 'react'
-import { ComponentName, ComponentSchema } from '../types/lowCodeComp.type'
+// import { useDrag } from 'react-dnd'
+import { ComponentSchema } from '../types/lowCodeComp.type'
+
+// TODO: split components into seperated files
+type ComponentProps = {
+  data?: string
+  style: Record<string, unknown>
+  id: string
+}
+
+const AudioComponent: React.FC<ComponentProps> = ({ data, style, id }) => {
+  return <audio src={data} style={style as CSSProperties} key={id} controls={true} />
+}
+
+const ImageComponent: React.FC<ComponentProps> = ({ data, style, id }) => {
+  return <img src={data} style={style as CSSProperties} key={id} />
+}
+
+const TextComponent: React.FC<ComponentProps> = ({ data, style, id }) => {
+  return (
+    <div style={style as CSSProperties} key={id}>
+      {data}
+    </div>
+  )
+}
+
+const VideoComponent: React.FC<ComponentProps> = ({ data, style, id }) => {
+  return <video src={data} style={style as CSSProperties} key={id} controls={true} />
+}
+
+const BoxComponent: React.FC<ComponentProps> = ({ data, style, id }) => {
+  return <video src={data} style={style as CSSProperties} key={id} controls={true} />
+}
 
 const renderJsonSchema = (schema: ComponentSchema): React.ReactNode => {
   const { name, data, style, id } = schema
-  switch (name) {
-    case ComponentName.AudioComponent:
-      return <audio src={data} style={style as CSSProperties} key={id} controls={true} />
-    case ComponentName.ImageComponent:
-      return <img src={data} style={style as CSSProperties} key={id} />
-    case ComponentName.TextComponent:
-      return (
-        <div style={style as CSSProperties} key={id}>
-          {data}
-        </div>
-      )
-    case ComponentName.VideoComponent:
-      return <video src={data} style={style as CSSProperties} key={id} controls={true} />
-    case ComponentName.BoxComponent:
-      return (
-        <div style={style as CSSProperties} key={id}>
-          {schema.children.map((ch) => {
-            return renderJsonSchema(ch)
-          })}
-        </div>
-      )
-    default:
-      return <></>
+  // const [, drag] = useDrag(() => ({
+  //   type: name
+  // }))
+  // if (name === ComponentName.BoxComponent) {
+  //   return (
+  //     <div style={style as CSSProperties} key={id}>
+  //       {schema.children?.map((ch) => {
+  //         return renderJsonSchema(ch)
+  //       })}
+  //     </div>
+  //   )
+  // }
+  const components = {
+    TextComponent,
+    ImageComponent,
+    VideoComponent,
+    AudioComponent,
+    BoxComponent
   }
+  const RenderComponent = components[name]
+  return <RenderComponent style={style} id={id} data={data ?? null} />
 }
 
 export default renderJsonSchema
