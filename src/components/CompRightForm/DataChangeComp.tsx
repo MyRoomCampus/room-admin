73a1d3368d2382d2c6b074dc92b2716c578f4551
liugangdao <liuzhi465@163.com>
2022-05-31 14:34:52 +0800

    feat: add-right-panel-form

--- src/components/CompRightForm/DataChangeComp.tsx
+++ src/components/CompRightForm/DataChangeComp.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext } from 'react'
 // import styles from './index.module.less'
 import { ComponentSchema } from '@//types/lowCodeComp.type'
 import AppContext from '@//store'
@@ -6,7 +6,7 @@ import ACTIONS from '@//reducer/actions'
 import _ from 'lodash'
 import { Button, Form, TextArea, Upload } from '@douyinfe/semi-ui'
 import { IconUpload } from '@douyinfe/semi-icons'
-import { FileItem } from '@douyinfe/semi-ui/lib/es/upload'
+// import { FileItem } from '@douyinfe/semi-ui/lib/es/upload'
 
 interface Props {
   comp: ComponentSchema
@@ -40,22 +40,12 @@ const ImageDataComponent: React.FC<Props> = (props) => {
   const { comp } = props
   //   const [preview, setPreview] = useState()
   // const style = { width: '100%' }
-  const [list, updateList] = useState([comp.data])
-
-  //   const onChange = ({ fileList, currentFile }) => {
-  //     console.log('onChange')
-  //     console.log(fileList)
-  //     console.log(currentFile)
-  //     const newFileList = [...fileList] // spread to get new array
-  //     updateList(newFileList)
-  //   }
-  console.log('comp:', list)
   const { Section } = Form
   return (
     <Section text={'图片内容'}>
       <Upload
         action="http://127.0.0.1:4523/mock/956583/resource?ContentType=multipart/form-data"
-        onChange={(e) => updateList(e.fileList.map((value: FileItem) => (value.url ? value.url : '')))}
+        // onChange={(e) => updateList(e.fileList.map((value: FileItem) => (value.url ? value.url : '')))}
         onPreviewClick={(e) => {
           console.log(e)
           const newSchema = _.cloneDeep(comp)
@@ -70,28 +60,66 @@ const ImageDataComponent: React.FC<Props> = (props) => {
           点击上传
         </Button>
       </Upload>
-      {/* {list.map((upFile, index) => {
-        return (
-          <div key={index}>
-            <img
-              src={upFile}
-              className={styles['previewImage']}
-              alt="preview"
-              onClick={() => {
-                console.log(index, list)
-                const newSchema = _.cloneDeep(comp)
-                newSchema.data = list[index]
-                dispatch({
-                  type: ACTIONS.UPDATE_COMPONENT,
-                  payload: newSchema
-                })
-              }}
-            />
-          </div>
-        )
-      })} */}
     </Section>
   )
 }
 
-export { TextDataComponent, ImageDataComponent }
+const VideoDataComponent: React.FC<Props> = (props) => {
+  const { dispatch } = useContext(AppContext)
+  const { comp } = props
+  //   const [preview, setPreview] = useState()
+  // const style = { width: '100%' }
+  const { Section } = Form
+  return (
+    <Section text={'视频内容'}>
+      <Upload
+        action="http://127.0.0.1:4523/mock/956583/resource?ContentType=multipart/form-data"
+        // onChange={(e) => updateList(e.fileList.map((value: FileItem) => (value.url ? value.url : '')))}
+        onPreviewClick={(e) => {
+          console.log(e)
+          const newSchema = _.cloneDeep(comp)
+          newSchema.data = e.url ? e.url : ''
+          dispatch({
+            type: ACTIONS.UPDATE_COMPONENT,
+            payload: newSchema
+          })
+        }}
+      >
+        <Button icon={<IconUpload />} theme="light">
+          点击上传
+        </Button>
+      </Upload>
+    </Section>
+  )
+}
+
+const AudioDataComponent: React.FC<Props> = (props) => {
+  const { dispatch } = useContext(AppContext)
+  const { comp } = props
+  //   const [preview, setPreview] = useState()
+  // const style = { width: '100%' }
+  const { Section } = Form
+  return (
+    <Section text={'录音内容'}>
+      <Upload
+        action="http://127.0.0.1:4523/mock/956583/resource?ContentType=multipart/form-data"
+        // onChange={(e) => updateList(e.fileList.map((value: FileItem) => (value.url ? value.url : '')))}
+        onPreviewClick={(e) => {
+          console.log(e)
+          const newSchema = _.cloneDeep(comp)
+          newSchema.data = e.url ? e.url : ''
+          dispatch({
+            type: ACTIONS.UPDATE_COMPONENT,
+            payload: newSchema
+          })
+        }}
+      >
+        <Button icon={<IconUpload />} theme="light">
+          点击上传
+        </Button>
+      </Upload>
+    </Section>
+  )
+}
+
+export { TextDataComponent, ImageDataComponent, VideoDataComponent, AudioDataComponent }
