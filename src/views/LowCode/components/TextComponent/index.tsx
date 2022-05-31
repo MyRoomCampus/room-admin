7b51b7582089c9b281c17e9f38346197e0ae77d9
vecpeng <841298391@qq.com>
2022-05-31 22:10:55 +0800

    fix: 文字组件编辑

--- src/views/LowCode/components/TextComponent/index.tsx
+++ src/views/LowCode/components/TextComponent/index.tsx
@@ -5,17 +5,27 @@ import AppContext from '@//store'
 import actions from '@//reducer/actions'
 import _ from 'lodash'
 import { TextComponent as TextComponentType } from '@//types/lowCodeComp.type'
+import { useBoolean } from 'ahooks'
 
 const TextComponent: React.FC<ComponentProps> = ({ schema }) => {
   console.log(schema)
   const { data, id, style } = schema
   const { dispatch } = useContext(AppContext)
+  const [canInput, { set }] = useBoolean(true)
   const handleTextInput = () => {
-    // const content = document.querySelector('[contenteditable]')?.textContent
+    const content = document.querySelector('#component-text')?.textContent
+    const range = document.createRange()
+    range.selectNodeContents(document.querySelector('#component-text') ?? document.createTextNode(''))
+    range.collapse(false)
+    const sel = window.getSelection()
+    if (sel) {
+      sel.removeAllRanges()
+      sel.addRange(range)
+    }
 
     const newSchema = _.cloneDeep(schema) as TextComponentType
-    // console.log(content, newSchema)
-    // newSchema.data = content ?? newSchema?.data
+    console.log(content, newSchema)
+    newSchema.data = content ?? newSchema?.data
     dispatch({
       type: actions.UPDATE_COMPONENT,
       payload: newSchema
@@ -24,7 +34,24 @@ const TextComponent: React.FC<ComponentProps> = ({ schema }) => {
 
   return (
     <div className={styles['component-text']} style={style as CSSProperties} key={id}>
-      <span onInput={handleTextInput} contentEditable="true" style={{ outline: 'none' }}>
+      <span
+        onCompositionStart={() => {
+          set(false)
+        }}
+        onCompositionEnd={() => {
+          console.log('compositionend')
+          set(true)
+          handleTextInput()
+        }}
+        onInput={() => {
+          if (canInput) {
+            handleTextInput()
+          }
+        }}
+        id="component-text"
+        contentEditable="true"
+        style={{ outline: 'none' }}
+      >
         {data}
       </span>
     </div>
